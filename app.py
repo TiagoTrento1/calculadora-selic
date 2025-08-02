2126cce78329f39e327d720c44133751aac48767
TiagoTrento1 <166037669+TiagoTrento1@users.noreply.github.com>
2025-08-01 22:24:49 -0300

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import base64
+
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
 import streamlit as st
 import pandas as pd
 import requests
